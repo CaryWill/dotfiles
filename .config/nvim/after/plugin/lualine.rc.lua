1942926865d2b56df3d3ee2d510cd6be635ddbc4
cary <cary@Macintosh.local>
2022-06-04 01:40:04 +0800

    fix: lualine

--- .config/nvim/after/plugin/lualine.rc.lua
+++ .config/nvim/after/plugin/lualine.rc.lua
@@ -5,8 +5,8 @@ lualine.setup {
   options = {
     icons_enabled = true,
     theme = 'nightfox',
-    section_separators = {left = '', right = ''},
-    component_separators = {left = '', right = ''},
+    section_separators = {left = '', right = ''},
+    component_separators = {left = '', right = ''},
     disabled_filetypes = {}
   },
   sections = {
