49c38238e1a429a39a735f215bb0f9aa38cbdbd7
wangzhangsheng(cary) <carywill.k@gmail.com>
2022-07-25 11:14:01 +0800

    fix: move to rect style

--- .config/nvim/after/plugin/lualine.rc.lua
+++ .config/nvim/after/plugin/lualine.rc.lua
@@ -4,8 +4,8 @@ if (not status) then return end
 lualine.setup {
   options = {
     theme = 'github_dark',
-    section_separators = {left = '', right = ''}, -- 
-    component_separators = {left = '', right = ''},
+    section_separators = {left = '', right = ''}, --   
+    component_separators = {left = '', right = ''},
     disabled_filetypes = {}
   },
   sections = {
