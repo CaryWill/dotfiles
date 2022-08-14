d81dd693b73f075b749c3486ff32a864dc3c7af5
cary <carywill.k@gmail.com>
2022-08-14 21:42:36 +0800

    fix: update bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@ brew install neovim --HEAD
 brew install ripgrep # 安装 fzf 的话需要
 brew tap daipeihust/tap && brew install im-select # 自动切换输入法
 
+npm install -g typescript typescript-language-server eslint_d prettier eslint diagnostic-languageserver
+
 # 设置软连接兼容 neovim
 ln -sf "$(pwd)/.config/nvim" ~/.config
 ln -sf "$(pwd)/.config/nvim/init.vim" ~/.vim/.vimrc
