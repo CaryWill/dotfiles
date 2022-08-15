5117b476a61b4389ab6ae581bb40dacbb235da57
wangzhangsheng(cary) <carywill.k@gmail.com>
2022-08-15 10:46:03 +0800

    fix: update bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,11 @@ brew install neovim --HEAD
 brew install ripgrep # 安装 fzf 的话需要
 brew tap daipeihust/tap && brew install im-select # 自动切换输入法
 
+# neovim lsp
 npm install -g typescript typescript-language-server eslint_d prettier eslint diagnostic-languageserver
+
+# zsh theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # 设置软连接兼容 neovim
 ln -sf "$(pwd)/.config/nvim" ~/.config
