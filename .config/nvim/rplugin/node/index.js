77517ff2ce69b2a7698519c20e6805ef0a9f07e5
cary <carywill.k@gmail.com>
2022-09-25 08:43:26 +0800

    fix: tmux left status

--- .config/nvim/rplugin/node/index.js
+++ .config/nvim/rplugin/node/index.js
@@ -1,11 +1,22 @@
+const fs = require('fs');
+
+
+fs.writeFile("/tmp/test", "Hey there!", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+}); 
+
 function onBufWrite() {
   console.log('Buffer written!');
 }
 
 module.exports = (plugin) => {
   function setLine() {
-    plugin.nvim.setLine(process.env.NVIM);
-    plugin.nvim.command('vsp');
+    const line = process.env.NVIM;
+    plugin.nvim.setLine(line);
+    //plugin.nvim.command('vsp');
   }
   plugin.registerCommand('SetMyLine', [plugin.nvim.buffer, setLine]);
   plugin.registerAutocmd('BufWritePre', onBufWrite, { pattern: '*' });
