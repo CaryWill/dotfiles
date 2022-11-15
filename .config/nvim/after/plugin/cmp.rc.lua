0b854751293c8cccdc66f9634afc58533cb23695
cary <CaryWill.K@gmail.com>
2022-11-15 22:41:37 +0800

    fix: change cmp border style

--- .config/nvim/after/plugin/cmp.rc.lua
+++ .config/nvim/after/plugin/cmp.rc.lua
@@ -7,6 +7,9 @@ cmp.setup({
       require('luasnip').lsp_expand(args.body)
     end,
   },
+  window = {
+    completion = cmp.config.window.bordered({ border = { "+", "-", "+", "|", "+", "-", "+", "|" }}),
+  },
   mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
