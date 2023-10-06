6724ee534b90bb6b7d2b47bc68215dba9ea9a9d1
carywill2 <carywill.k1@gmail.com>
2023-10-06 17:08:29 +0800

    fix: update bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,12 +12,12 @@ brew install reattach-to-user-namespace
 # Install Squirrel Input source(we can switch input sources using 
 # `shift` command
 # https://zhyq118.com/2018/11/11/%E9%BC%A0%E9%A1%BB%E7%AE%A1%E6%8C%82%E6%8E%A5%E5%B0%8F%E9%B9%A4%E5%8F%8C%E6%8B%BC/
-brew install --cask squirrel
+# brew install --cask squirrel
 # install xiaohe source
-curl -fsSL https://raw.githubusercontent.com/rime/plum/master/rime-install | bash
-cd plum
-bash rime-install double-pinyin
-cd ..
+# curl -fsSL https://raw.githubusercontent.com/rime/plum/master/rime-install | bash
+# cd plum
+# bash rime-install double-pinyin
+# cd ..
 # manually click Squirrel's deploy button in the menu bar
 
 # 4. vim's backup directory
@@ -34,8 +34,8 @@ ln -sf "$(pwd)/dotfiles/.bashrc" ~/.bashrc
 ln -sf "$(pwd)/dotfiles/.zshrc" ~/.zshrc
 ln -sf "$(pwd)/coc-settings.json" ~/.vim/coc-settings.json
 #ln -sf "$(pwd)/.config/com.apple.Terminal.plist" ~/Library/Preferences/com.apple.Terminal.plist
-ln -sf "$(pwd)/rime/default.custom.yaml" ~/Library/Rime/default.custom.yaml
-ln -sf "$(pwd)/rime/squirrel.custom.yaml" ~/Library/Rime/squirrel.custom.yaml
+# ln -sf "$(pwd)/rime/default.custom.yaml" ~/Library/Rime/default.custom.yaml
+# ln -sf "$(pwd)/rime/squirrel.custom.yaml" ~/Library/Rime/squirrel.custom.yaml
 
 # 7. 安装 vim 插件
 vim +PlugInstall +qall
