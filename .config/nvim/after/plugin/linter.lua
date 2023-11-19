11cdecb3c3afa448fa74ef1c07420be91c85fa33
carywill2 <carywill.k1@gmail.com>
2023-11-19 17:12:39 +0800

    fix: multiple fixer

--- .config/nvim/after/plugin/linter.lua
+++ .config/nvim/after/plugin/linter.lua
@@ -1,4 +1,9 @@
 require("conform").setup({
+    formatters_by_ft = {
+        -- Conform will run multiple formatters sequentially
+        -- Use a sub-list to run only the first available formatter
+        javascript = {{"prettier", "eslint"}}
+    },
     format_on_save = {
         -- These options will be passed to conform.format()
         timeout_ms = 500,
