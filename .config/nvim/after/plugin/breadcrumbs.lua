6e3767e1b14bd8f042e434b438f5233cb4f7ef68
carywill2 <carywill.k1@gmail.com>
2023-11-22 20:11:25 +0800

    fix: perf

--- .config/nvim/after/plugin/breadcrumbs.lua
+++ .config/nvim/after/plugin/breadcrumbs.lua
@@ -1,1 +1,19 @@
-require("barbecue").setup({})
+-- triggers CursorHold event faster
+vim.opt.updatetime = 200
+
+require("barbecue").setup({
+})
+
+vim.api.nvim_create_autocmd({
+  "WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+  "BufWinEnter",
+  "CursorHold",
+  "InsertLeave",
+  -- include this if you have set `show_modified` to `true`
+  "BufModifiedSet",
+}, {
+  group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+  callback = function()
+    require("barbecue.ui").update()
+  end,
+})
