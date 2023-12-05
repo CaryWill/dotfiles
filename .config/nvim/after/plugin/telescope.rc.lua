cf3730735f338a99822bf6202908fbc72031175f
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2023-12-05 12:30:40 +0800

    fix: telescope

--- .config/nvim/after/plugin/telescope.rc.lua
+++ .config/nvim/after/plugin/telescope.rc.lua
@@ -5,22 +5,22 @@ local telescope = require('telescope')
 local actions = require('telescope.actions')
 
 telescope.setup {
-    defaults = {
-        file_ignore_patterns = {".git/", "node_modules/", ".node/", ".vscode/", ".husky/", "package-lock.json",
-                                "yarn.lock"},
-        mappings = {
-            n = {
-                ["q"] = actions.close
-            },
-            i = {
-                ["<C-s>"] = actions.file_split,
-                ["<C-v>"] = actions.file_vsplit,
-                ["<C-t>"] = actions.file_tab
-            }
-        },
-        layout_config = {
-            preview_width = 0.5
-        },
-        vimgrep_arguments = {'rg', '--color=never', '--no-heading', '--line-number', '--column', '--smart-case'}
-    }
+  defaults = {
+    file_ignore_patterns = { ".git/", "node_modules/", ".node/", ".vscode/", ".husky/", "package-lock.json",
+      "yarn.lock" },
+    mappings = {
+      n = {
+        ["q"] = actions.close
+      },
+      i = {
+        ["<C-s>"] = actions.file_split,
+        ["<C-v>"] = actions.file_vsplit,
+        ["<C-t>"] = actions.file_tab
+      }
+    },
+    layout_config = {
+      -- preview_width = 0.5
+    },
+    -- vimgrep_arguments = { 'rg', '--color=never', '--no-heading', '--line-number', '--column', '--smart-case' }
+  }
 }
