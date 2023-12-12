d0fcf489e38cfd0e66052d3a3cef688893ef1bc9
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2023-12-12 13:55:15 +0800

    fix: mini plugins pcall

--- .config/nvim/after/plugin/mini.lua
+++ .config/nvim/after/plugin/mini.lua
@@ -1,4 +1,15 @@
-require('mini.indentscope').setup()
-require('mini.pairs').setup()
-require('mini.surround').setup()
-require('mini.ai').setup()
+local status, indentscope = pcall(require, "mini.indentscope")
+if (not status) then return end
+indentscope.setup()
+
+local status1, pairs = pcall(require, "mini.pairs")
+if (not status1) then return end
+pairs.setup()
+
+local status2, surround = pcall(require, "mini.surround")
+if (not status2) then return end
+surround.setup()
+
+local status3, ai = pcall(require, "mini.ai")
+if (not status3) then return end
+ai.setup()
