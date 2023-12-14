bdf2f6506a1f8423613fa561f1367cdf659c05d4
carywill2 <carywill.k1@gmail.com>
2023-12-14 23:35:32 +0800

    fix: terminal float size

--- .config/nvim/after/plugin/jest.rc.lua
+++ .config/nvim/after/plugin/jest.rc.lua
@@ -1,3 +1,9 @@
 require("jester").setup({
-  terminal_cmd = ":FloatermNew"
+  -- terminal_cmd = ':lua require("FTerm").toggle()'
+  terminal_cmd = ':FloatermNew'
 })
+
+vim.g.floaterm_width = 0.9
+vim.g.floaterm_height = 0.9
+-- A for alt key
+vim.keymap.set('t', '<A-i>', '<C-\\><C-n><CMD>FloatermToggle<CR>')
