07a43d486792e06b2163cfc930c1bb92299776f9
carywill2 <carywill.k1@gmail.com>
2023-12-16 22:27:34 +0800

    fix: update

--- .config/nvim/after/plugin/telescope.rc.lua
+++ .config/nvim/after/plugin/telescope.rc.lua
@@ -16,9 +16,6 @@ telescope.setup {
         ["<C-t>"] = actions.file_tab
       }
     },
-    layout_config = {
-      -- preview_width = 0.5
-    },
     -- https://github.com/BurntSushi/ripgrep/issues/299#issuecomment-270102901
     -- search ( instead of \( when searching
     vimgrep_arguments = { 'rg', '--color=never', '--no-heading', '--line-number', '--column', '--smart-case', '--fixed-strings', '--hidden' },
@@ -30,14 +27,7 @@ telescope.setup {
         case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
         -- the default case_mode is "smart_case"
       },
-      undo = {
-        side_by_side = true,
-        layout_strategy = "vertical",
-        layout_config = {
-          preview_height = 0.8,
-        },
-
-      },
+      undo = {},
       harpoon = {},
       frecency = {
         show_scores = true,
