e221eb617fff9ee8ca8db22456d53b9f186eb493
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2023-12-19 16:49:21 +0800

    fix: update

--- .config/nvim/after/plugin/telescope.rc.lua
+++ .config/nvim/after/plugin/telescope.rc.lua
@@ -3,8 +3,29 @@ if (not status) then return end
 
 local actions = require('telescope.actions')
 
+local previewers = require('telescope.previewers')
+local previewers_utils = require('telescope.previewers.utils')
+
+-- Faster preview large file
+-- current condition: preview large file stucks
+local max_size = 100000
+local truncate_large_files = function(filepath, bufnr, opts)
+  opts = opts or {}
+  filepath = vim.fn.expand(filepath)
+  vim.loop.fs_stat(filepath, function(_, stat)
+    if not stat then return end
+    if stat.size > max_size then
+      local cmd = { "head", "-c", max_size, filepath }
+      previewers_utils.job_maker(cmd, bufnr, opts)
+    else
+      previewers.buffer_previewer_maker(filepath, bufnr, opts)
+    end
+  end)
+end
+
 telescope.setup {
   defaults = {
+    buffer_previewer_maker = truncate_large_files,
     file_ignore_patterns = { ".git/", "node_modules/", ".node/", ".vscode/", ".husky/", "package-lock.json", "yarn.lock" },
     mappings = {
       n = {
