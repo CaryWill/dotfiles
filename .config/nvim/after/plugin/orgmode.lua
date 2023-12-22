6ca387ef1ca1ae8c08df7447283b39ed576bf193
carywill2 <carywill.k1@gmail.com>
2023-12-22 22:54:44 +0800

    fix: update no orgmode

--- .config/nvim/after/plugin/orgmode.lua
+++ .config/nvim/after/plugin/orgmode.lua
@@ -1,4 +1,7 @@
 -- Load custom treesitter grammar for org filetype
+local status, plugin = pcall(require, "orgmode")
+if (not status) then return end
+
 require("orgmode").setup_ts_grammar()
 
 -- Treesitter configuration
