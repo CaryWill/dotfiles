a0fabb9387220b150a03fa612dacc9ca7298f703
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2023-12-22 16:06:39 +0800

    fix: inactive lualne full path

--- .config/nvim/after/plugin/lualine.rc.lua
+++ .config/nvim/after/plugin/lualine.rc.lua
@@ -21,4 +21,12 @@ plugin.setup({
 			"filetype",
 		},
 	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { { "filename", path = 1 } },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
 })
