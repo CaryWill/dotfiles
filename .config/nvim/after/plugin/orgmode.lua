70095d69988377d2930d2fafa3269bfe4d61acca
carywill2 <carywill.k1@gmail.com>
2023-12-23 18:07:08 +0800

    fix: udpate

--- .config/nvim/after/plugin/orgmode.lua
+++ .config/nvim/after/plugin/orgmode.lua
@@ -4,5 +4,8 @@ if not status then
 	return
 end
 
+-- NOTE: there's a bug in harpoon plugin
+-- if you open the file using harpoon
+-- the fold on orgmode will not work properly
 require("orgmode").setup_ts_grammar()
 require("orgmode").setup({})
