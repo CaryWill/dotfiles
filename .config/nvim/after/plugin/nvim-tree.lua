f688b04817396d36a3a0eed1c7e4abbd6901d04a
carywill2 <carywill.k1@gmail.com>
2023-12-23 18:05:48 +0800

    fix: update

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -20,7 +20,7 @@ local function on_attach(bufnr)
   vim.keymap.set('n', '<C-v>', api.node.open.vertical, opts('Open: Vertical Split'))
   vim.keymap.set('n', '<C-s>', api.node.open.horizontal, opts('Open: Horizontal Split'))
   vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
-  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+  -- vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
   vim.keymap.set('n', '<Tab>', api.node.open.preview, opts('Open Preview'))
   vim.keymap.set('n', '>', api.node.navigate.sibling.next, opts('Next Sibling'))
   vim.keymap.set('n', '<', api.node.navigate.sibling.prev, opts('Previous Sibling'))
@@ -50,14 +50,14 @@ local function on_attach(bufnr)
   vim.keymap.set('n', 'J', api.node.navigate.sibling.last, opts('Last Sibling'))
   vim.keymap.set('n', 'K', api.node.navigate.sibling.first, opts('First Sibling'))
   vim.keymap.set('n', 'm', api.marks.toggle, opts('Toggle Bookmark'))
-  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
-  vim.keymap.set('n', 'O', api.node.open.no_window_picker, opts('Open: No Window Picker'))
+  -- vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<CR>', api.node.open.no_window_picker, opts('Open: No Window Picker'))
   vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
   vim.keymap.set('n', 'P', api.node.navigate.parent, opts('Parent Directory'))
   vim.keymap.set('n', 'q', api.tree.close, opts('Close'))
   vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
   vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
-  vim.keymap.set('n', 's', api.node.run.system, opts('Run System'))
+  vim.keymap.set('n', 'o', api.node.run.system, opts('Run System'))
   vim.keymap.set('n', 'S', api.tree.search_node, opts('Search'))
   vim.keymap.set('n', 'u', api.fs.rename_full, opts('Rename: Full Path'))
   vim.keymap.set('n', 'U', api.tree.toggle_custom_filter, opts('Toggle Filter: Hidden'))
@@ -208,7 +208,7 @@ require("nvim-tree").setup { -- BEGIN_DEFAULT_OPTS
     ignore_list = {},
   },
   system_open = {
-    cmd = "",
+    cmd = "open",
     args = {},
   },
   git = {
