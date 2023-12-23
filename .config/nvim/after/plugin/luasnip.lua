5b168e930624dc7c764a5be6f30d43a70da4026f
carywill2 <carywill.k1@gmail.com>
2023-12-23 13:38:00 +0800

    f

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -1,22 +1,22 @@
 local status, ls = pcall(require, "luasnip")
 if not status then
-	return
+  return
 end
 
 vim.keymap.set({ "i" }, "<C-K>", function()
-	ls.expand()
+  ls.expand()
 end, { silent = true })
 vim.keymap.set({ "i", "s" }, "<C-L>", function()
-	ls.jump(1)
+  ls.jump(1)
 end, { silent = true })
 vim.keymap.set({ "i", "s" }, "<C-J>", function()
-	ls.jump(-1)
+  ls.jump(-1)
 end, { silent = true })
 
 vim.keymap.set({ "i", "s" }, "<C-E>", function()
-	if ls.choice_active() then
-		ls.change_choice(1)
-	end
+  if ls.choice_active() then
+    ls.change_choice(1)
+  end
 end, { silent = true })
 
 -- using all html snippets in react files
@@ -29,9 +29,6 @@ require("luasnip").filetype_extend("typescript", { "html" })
 
 -- https://github.com/rafamadriz/friendly-snippets/wiki/Javascript,-Typescript,-Javascriptreact,-Typescriptreact
 -- load pre defined snippets
--- require("luasnip.loaders.from_vscode").lazy_load()
-local snippet_dir = vim.fn.fnamemodify(vim.env.MYVIMRC, ":p:h") .. "/snippets"
-
-local paths = {}
-table.insert(paths, 1, snippet_dir)
-require("luasnip.loaders.from_vscode").lazy_load({ paths = paths })
+require("luasnip.loaders.from_vscode").lazy_load()
+-- local snippet_dir = vim.fn.fnamemodify(vim.env.MYVIMRC, ":p:h") .. "/snippets"
+-- require("luasnip.loaders.from_vscode").lazy_load({ paths = "./snippets" })
