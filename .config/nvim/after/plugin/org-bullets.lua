da391db98cc54cf56dfdb81442026ff45cea6dda
carywill2 <carywill.k1@gmail.com>
2023-12-24 13:04:21 +0800

    f

--- .config/nvim/after/plugin/org-bullets.lua
+++ .config/nvim/after/plugin/org-bullets.lua
@@ -1,5 +1,14 @@
 local status, harpoon = pcall(require, "org-bullets")
-if (not status) then return end
+if not status then
+	return
+end
 
 require("org-bullets").setup({
+	symbols = {
+		checkboxes = {
+			half = { "-", "OrgTSCheckboxHalfChecked" },
+			done = { "✓", "OrgDone" },
+			todo = { " ", "OrgTODO" },
+		},
+	},
 })
