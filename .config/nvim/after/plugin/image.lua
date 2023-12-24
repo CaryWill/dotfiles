d1a370b4bf982d0acf8db46e3ac08f28f4cebcc7
carywill2 <carywill.k1@gmail.com>
2023-12-24 21:02:46 +0800

    fix: update

--- .config/nvim/after/plugin/image.lua
+++ .config/nvim/after/plugin/image.lua
@@ -23,7 +23,7 @@ require("image").setup({
 		},
 		org = {
 			enabled = true,
-			clear_in_insert_mode = false,
+			clear_in_insert_mode = true,
 			download_remote_images = true,
 			only_render_image_at_cursor = false,
 			filetypes = { "org" },
