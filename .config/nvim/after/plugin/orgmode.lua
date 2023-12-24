ec11d3fb941ddeef63318bac4eccc15ba4fe0fb3
carywill2 <carywill.k1@gmail.com>
2023-12-24 21:49:31 +0800

    fix: updat

--- .config/nvim/after/plugin/orgmode.lua
+++ .config/nvim/after/plugin/orgmode.lua
@@ -31,6 +31,7 @@ require("orgmode").setup({
 	-- org_log_into_drawer = "LOGBOOK",
 	org_startup_folded = "showeverything",
 	org_custom_exports = {
+    -- NOTE: there's also other options, like to html then to markdown
 		g = {
 			label = "Export to gfm markdown personal refined",
 			action = function(exporter)
