015d4f16e517368972e10b809dfaca415acb3e04
carywill2 <carywill.k1@gmail.com>
2023-12-26 21:20:48 +0800

    fix: update

--- .config/nvim/after/plugin/bigfile.lua
+++ .config/nvim/after/plugin/bigfile.lua
@@ -4,9 +4,20 @@ if (not status) then return end
 
 require("bigfile").setup({
 	-- size of the file in MiB, the plugin round file sizes to the closest MiB
-	filesize = 1,
+	filesize = 0.1,
 	-- autocmd pattern or function see <### Overriding the detection of big files>
-	pattern = { "*" },
+	-- pattern = { "*" },
+  -- detect long python files
+  pattern = function(bufnr, filesize_mib)
+    -- you can't use `nvim_buf_line_count` because this runs on BufReadPre
+    local file_contents = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
+    local file_length = #file_contents
+    -- local filetype = vim.filetype.match({ buf = bufnr })
+    -- 0.5mb
+    if file_length > 5000 then
+      return true
+    end
+  end, 
 	features = { -- features to disable
 		"indent_blankline",
 		"illuminate",
