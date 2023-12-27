d497eef0de5f753a20f8c7518797cda6b0e1c820
carywill2 <carywill.k1@gmail.com>
2023-12-27 22:50:34 +0800

    update

--- .config/nvim/after/plugin/orgmode.lua
+++ .config/nvim/after/plugin/orgmode.lua
@@ -24,7 +24,7 @@ end
 require("orgmode").setup_ts_grammar()
 local icloud_org_dir_path = "~/Library/Mobile Documents/com~apple~CloudDocs/Plain Org/"
 require("orgmode").setup({
-	org_agenda_files = { icloud_org_dir_path .. "**/*" },
+	org_agenda_files = { icloud_org_dir_path .. "gtd/agenda/**/*" },
 	org_default_notes_file = icloud_org_dir_path .. "default.org",
 	org_capture_templates = {
 		T = {
