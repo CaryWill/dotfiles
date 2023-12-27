e0d07717efd40594d9fd62dab720f5b4283a6c06
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2023-12-27 17:34:06 +0800

    organize test file

--- .config/nvim/ftplugin/org.lua
+++ .config/nvim/ftplugin/org.lua
@@ -1,4 +1,8 @@
 vim.opt_local.foldenable = true
 vim.opt_local.conceallevel = 2
 vim.opt_local.concealcursor = "nc"
+-- vim.opt_local.wrap= true
+-- vim.opt_local.linebreak= false
+-- vim.opt_local.textwidth= 0
+-- vim.opt_local.wrapmargin= 0
 vim.opt_local.shellslash = true
