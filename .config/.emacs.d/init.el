bab5ca270cb4d34348b0653c9be0549d750b5eff
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2023-12-29 12:59:49 +0800

    emacs config

---
+++ .config/.emacs.d/init.el
@@ -0,0 +1,21 @@
+;; http://xahlee.info/emacs/emacs/emacs_package_system.html
+(when (>= emacs-major-version 24)
+  (progn
+    ;; load emacs 24's package system.
+    (require 'package)
+    ;; Add MELPA repository.
+    (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
+  (when (< emacs-major-version 27) (package-initialize)))
+
+;; https://emacs.stackexchange.com/questions/73403/install-packages-with-one-command
+
+(setq
+ ;; archives to install packages from
+ package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                    ("melpa" . "https://melpa.org/packages/"))
+ ;; packages to have installed
+ package-selected-packages '(magit))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+(package-install-selected-packages 'noconfirm)
