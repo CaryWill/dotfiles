f7b3f8aeb1fa54eb382196d8c7fba91bc3825e0d
carywill2 <carywill.k1@gmail.com>
2023-12-30 16:23:44 +0800

    update

--- .config/nvim/after/plugin/orgmode.lua
+++ .config/nvim/after/plugin/orgmode.lua
@@ -90,6 +90,6 @@ require("orgmode").setup({
 	-- org_hide_emphasis_markers = true,
 	org_agenda_text_search_extra_files = { "agenda-archives" },
 	-- org_agenda_start_on_weekday = false,
-	org_blank_before_new_entry = { heading = false, plain_list_item = false },
+	-- org_blank_before_new_entry = { heading = false, plain_list_item = false },
 	-- emacs_config = { executable_path = "emacs", config_path = "$HOME/.emacs.d/init.el" },
 })
