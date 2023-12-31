846e9fd6134fc795d6b5944734714a97e77a35af
carywill2 <carywill.k1@gmail.com>
2023-12-31 15:45:34 +0800

    org

--- .config/nvim/ftplugin/org.lua
+++ .config/nvim/ftplugin/org.lua
@@ -1,6 +1,6 @@
 vim.opt_local.foldenable = true
--- vim.opt_local.conceallevel = 2
--- vim.opt_local.concealcursor = "nc"
+vim.opt_local.conceallevel = 2
+vim.opt_local.concealcursor = "nc"
 -- vim.opt_local.wrap = true
 -- vim.opt_local.linebreak= false
 -- vim.opt_local.textwidth= 0
