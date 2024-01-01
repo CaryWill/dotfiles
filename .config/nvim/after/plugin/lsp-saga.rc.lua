7767c9fa1dd2ac2f4cadbdc4308707952512bba3
carywill2 <carywill.k1@gmail.com>
2024-01-01 14:13:58 +0800

    write

--- .config/nvim/after/plugin/lsp-saga.rc.lua
+++ .config/nvim/after/plugin/lsp-saga.rc.lua
@@ -1,35 +1,38 @@
 local status, plugin = pcall(require, "lspsaga")
-if (not status) then return end
+if not status then
+	return
+end
 
 plugin.setup({
-  ui = {
-    winblend = 10,
-    border = 'rounded',
-    code_action = "",
-    actionfix = "💡  ",
-    devicon = false
-  },
-  symbol_in_winbar = {
-    enable = false
-  },
-  diagnostic = {
-    show_code_action = false,
-    extend_relatedInformation = true,
-  },
-  outline = {
-    auto_preview = false
-  }
+	ui = {
+		winblend = 10,
+		border = "rounded",
+		code_action = "",
+		actionfix = "💡  ",
+		devicon = false,
+	},
+	symbol_in_winbar = {
+		enable = false,
+	},
+	diagnostic = {
+		show_code_action = false,
+		extend_relatedInformation = true,
+	},
+	outline = {
+		auto_preview = false,
+	},
 })
 
 -- diagnostics navigation
 vim.keymap.set("n", "[e", function()
-  require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+	require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
 end)
 vim.keymap.set("n", "]e", function()
-  require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
+	require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
 end)
 
 -- code action
+-- grammarly can use it to do a auto correct, or eslint,...
 vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
 -- outline
 -- vim.keymap.set({ "n", "v" }, "<leader>o", "<cmd>Lspsaga outline<CR>")
