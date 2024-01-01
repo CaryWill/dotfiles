f111a3a82b18d370355b0f5df78148d30a45d82b
carywill2 <carywill.k1@gmail.com>
2024-01-01 21:58:42 +0800

    google trans

--- .config/nvim/after/plugin/translate.lua
+++ .config/nvim/after/plugin/translate.lua
@@ -1,15 +1,16 @@
 local status, plugin = pcall(require, "translate")
 if not status then
-	return
+  return
 end
 
 require("translate").setup({
-	default = {
-		parse_before = "natural",
-		command = "google",
-		parse_after = "head",
-		output = "replace",
-	},
+  default = {
+    parse_before = "natural",
+    command = "google",
+    parse_after = "head",
+    output = "replace",
+  },
 })
-
-vim.keymap.set("v", "<leader>gt", ":Translate en<CR>")
+-- vim.keymap.set("v", "<leader>gt", ":Translate en -output=replace<CR>")
+vim.keymap.set("v", "<leader>gg", ":Translate en<CR>", { silent = true })
+vim.keymap.set("v", "<leader>gp", ":Translate en -output=floating<CR>", { silent = true })
