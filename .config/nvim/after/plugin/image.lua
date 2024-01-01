599d936df9c95e270ee68d8966e71313f54a6f41
carywill2 <carywill.k1@gmail.com>
2024-01-01 11:30:04 +0800

    org

--- .config/nvim/after/plugin/image.lua
+++ .config/nvim/after/plugin/image.lua
@@ -23,9 +23,9 @@ require("image").setup({
 		},
 		org = {
 			enabled = true,
-			clear_in_insert_mode = false,
+			clear_in_insert_mode = true,
 			download_remote_images = true,
-			only_render_image_at_cursor = false,
+			only_render_image_at_cursor = true,
 			filetypes = { "org" },
 		},
 	},
