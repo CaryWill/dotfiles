2d24b159c481877fc9307850b8c54de6bdc0e9e2
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-01-03 12:46:34 +0800

    writing

--- .config/nvim/ftplugin/org.lua
+++ .config/nvim/ftplugin/org.lua
@@ -2,7 +2,9 @@ vim.opt_local.foldenable = true
 vim.opt_local.conceallevel = 2
 vim.opt_local.concealcursor = "nc"
 vim.opt_local.spell = true
-vim.opt_local.spelllang = "en_us"
+-- If 'spelllang' includes "cjk", these characters are not marked as errors.
+-- https://neovim.io/doc/user/spell.html#spell-cjk
+vim.opt_local.spelllang = "en_us,cjk"
 vim.opt_local.colorcolumn = "80"
 vim.opt_local.wrap = false
 -- vim.opt_local.wrap = true
