cc987d6fc75a465dc6a8f7b6cd0631c00d43451d
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-01-04 19:01:22 +0800

    autoread

--- .config/nvim/after/plugin/autocmd.lua
+++ .config/nvim/after/plugin/autocmd.lua
@@ -66,6 +66,7 @@
 
 -- from here
 -- https://neovim.discourse.group/t/a-lua-based-auto-refresh-buffers-when-they-change-on-disk-function/2482/3
+-- https://gitlab.com/jokeyrhyme/dotfiles/-/blob/main/config/nvim/init.lua#L5-11
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
 	command = "if mode() != 'c' | checktime | endif",
 	pattern = { "*" },
