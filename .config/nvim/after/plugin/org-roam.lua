57ad7fac2f22c3fb070fd105cd1fa10c577d58e1
carywill2 <carywill.k1@gmail.com>
2024-01-07 20:00:04 +0800

    add readme

--- .config/nvim/after/plugin/org-roam.lua
+++ .config/nvim/after/plugin/org-roam.lua
@@ -6,11 +6,11 @@ end
 local org_roam = require("org-roam")
 org_roam.setup({
 	-- required
-	org_roam_directory = "~/OrgRoam",
-
+	-- org_roam_directory = "~/OrgRoam",
+	org_roam_directory = "~/Library/Mobile Documents/com~apple~CloudDocs/Plain Org/zettelkasten/fleeting",
 	-- required (not checked if present)
 	org_roam_database_file = "~/.config/emacs/org-roam.db",
 })
 
-vim.keymap.set("n", "<Leader>rf", org_roam.org_roam_node_find)
-vim.keymap.set("n", "<Leader>rc", org_roam.org_roam_capture)
+vim.keymap.set("n", "<Leader>zf", org_roam.org_roam_node_find)
+vim.keymap.set("n", "<Leader>zc", org_roam.org_roam_capture)
