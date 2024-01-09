092a31af5bed5a4133df75091923e8198ba1214d
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-01-09 17:05:39 +0800

    org_roam_capture_directory

--- .config/nvim/after/plugin/org-roam.lua
+++ .config/nvim/after/plugin/org-roam.lua
@@ -7,9 +7,9 @@ local org_roam = require("org-roam")
 org_roam.setup({
 	-- required
 	-- org_roam_directory = "~/OrgRoam",
-	org_roam_directory = "~/Library/Mobile Documents/com~apple~CloudDocs/Plain Org/zettelkasten",
-	-- file will created at
-	org_roam_capture_directory = "~/Library/Mobile Documents/com~apple~CloudDocs/Plain Org/zettelkasten/fleeting",
+	org_roam_directory = "~/Library/Mobile Documents/com~apple~CloudDocs/Plain Org",
+	-- file will created at org_roam_capture_directory or org_roam_directory
+	-- org_roam_capture_directory = "~/Library/Mobile Documents/com~apple~CloudDocs/Plain Org",
 	-- required (not checked if present)
 	org_roam_database_file = "~/.config/emacs/org-roam.db",
 })
