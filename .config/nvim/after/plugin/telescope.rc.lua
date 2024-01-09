8be25859c3240be0bf8a73d27e8f98575c6789ce
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-01-09 17:11:11 +0800

    org_roam_capture_directory

--- .config/nvim/after/plugin/telescope.rc.lua
+++ .config/nvim/after/plugin/telescope.rc.lua
@@ -63,7 +63,7 @@ telescope.setup({
 				-- ["<C-y>"] = require("telescope.actions.layout").toggle_preview,
 				["<C-j>"] = actions.move_selection_next,
 				["<C-k>"] = actions.move_selection_previous,
-				["<ESC>"] = actions.close,
+				-- ["<ESC>"] = actions.close,
 				["<C-[>"] = actions.close,
 				-- ["q"] = actions.close,
 			},
