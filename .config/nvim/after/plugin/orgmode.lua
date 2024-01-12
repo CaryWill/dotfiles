eb87f52be2030d14470225936ede6b396c360bba
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-01-12 11:36:18 +0800

    add orgmode todo state

--- .config/nvim/after/plugin/orgmode.lua
+++ .config/nvim/after/plugin/orgmode.lua
@@ -59,7 +59,7 @@ require("orgmode").setup({
 	},
 	-- org_log_into_drawer = "LOGBOOK",
 	org_startup_folded = "showeverything",
-	org_todo_keywords = { "TODO(t)", "WAITING(w)", "PROGRESS(p)", "|", "DONE(d)" },
+	org_todo_keywords = { "TODO(t)", "WAITING(w)", "PROGRESS(p)", "CONFIRMING(c)", "|", "DONE(d)" },
 	org_custom_exports = {
 		-- NOTE: there's also other options, like to html then to markdown
 		g = {
