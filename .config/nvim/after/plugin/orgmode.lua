67e87be59815f3ae0bc572bc390b83c58027213e
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-01-13 16:16:28 +0800

    add zettle folder

--- .config/nvim/after/plugin/orgmode.lua
+++ .config/nvim/after/plugin/orgmode.lua
@@ -34,13 +34,6 @@ require("orgmode").setup({
 		-- 	template = "* TODO %?\n %u",
 		-- 	target = icloud_org_dir_path .. "todo.org",
 		-- },
-		-- org-roam-zettle
-		r = {
-			description = "Notes",
-			template = "* %?\n %u",
-			target = icloud_org_dir_path .. "reference/%t.org",
-		},
-		-- orgmode-refile
 		n = {
 			description = "Notes",
 			template = "* %?\n %u",
