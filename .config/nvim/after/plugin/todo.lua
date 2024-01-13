1735ae0f99e20c3ad570438a65dacdeb039dc260
carywill2 <carywill.k1@gmail.com>
2024-01-13 22:13:19 +0800

    u

--- .config/nvim/after/plugin/todo.lua
+++ .config/nvim/after/plugin/todo.lua
@@ -2,10 +2,11 @@ local status, plugin = pcall(require, "todo-comments")
 if not status then
 	return
 end
-
+-- TODO: test
 plugin.setup({
 	signs = false,
 	highlight = {
 		exclude = { "org" },
+		keyword = "bg",
 	},
 })
