bbe886373d4fa9670ebdacc20059b67c47ac7062
carywill2 <carywill.k1@gmail.com>
2024-01-14 22:31:12 +0800

    move cursor

--- .config/nvim/after/plugin/org-roam.lua
+++ .config/nvim/after/plugin/org-roam.lua
@@ -15,8 +15,26 @@ org_roam.setup({
 	-- paths you want to save zettle when do a capture
 	org_roam_zettle_paths = { "reference", "main", "articles" },
 })
+function openURL(url)
+	local command = string.format("open %s", url)
+	local result = os.execute(command)
+
+	if result == true then
+		print("URL opened successfully.")
+	else
+		print("Error opening URL.")
+	end
+end
+
+local open_org_roam_ui = function()
+	-- org-roam-ui default on 3000 port
+	local urlToOpen = "http://localhost:3000"
+	openURL(urlToOpen)
+	vim.fn.InitWs()
+end
 
 vim.keymap.set("n", "<Leader>nf", org_roam.org_roam_node_find)
 vim.keymap.set("n", "<Leader>ni", org_roam.org_roam_node_insert)
 vim.keymap.set("n", "<Leader>nl", org_roam.org_roam_buffer_toggle)
 vim.keymap.set("n", "<Leader>zn", org_roam.org_roam_capture)
+vim.keymap.set("n", "<Leader>zo", open_org_roam_ui)
