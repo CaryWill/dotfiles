9e602276fe67a2a08225b9ac1868691eb80b1af6
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-01-30 21:35:05 +0800

    fix: auto change theme

--- .config/nvim/after/plugin/autocmd.lua
+++ .config/nvim/after/plugin/autocmd.lua
@@ -8,3 +8,11 @@ vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGai
 	command = "if mode() != 'c' | checktime | endif",
 	pattern = { "*" },
 })
+
+-- 9AM - 18 AM 自动切换主题
+local hr = tonumber(os.date("%H", os.time()))
+if hr > 9 and hr < 18 then
+	vim.opt.background = "dark"
+else -- night
+	vim.opt.background = "dark"
+end
