9f026a5731b0d32802113420749f7bcb45e497b3
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-02-01 15:44:39 +0800

    fix: update

--- .config/nvim/after/plugin/bufferline.lua
+++ .config/nvim/after/plugin/bufferline.lua
@@ -1,40 +1,56 @@
 local status, plugin = pcall(require, "bufferline")
-if (not status) then return end
+if not status then
+	return
+end
 
 plugin.setup({
-  options = {
-    show_duplicate_prefix = true,
-    close_command = function(n) require("mini.bufremove").delete(n, false) end,
-    right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
-    diagnostics = "nvim_lsp",
-    always_show_bufferline = false,
-    offsets = {
-      {
-        filetype = "NvimTree",
-        -- filetype = "neo-tree",
-        text = "File-explorer",
-        highlight = "Directory",
-        text_align = "left",
-      },
-    },
-    hover = {
-      enabled = true,
-      delay = 100,
-      reveal = { 'close' }
-    },
-    indicator = {
-      style = 'underline',
-    },
-    numbers = function(opts)
-      return string.format('%s', opts.raise(opts.ordinal))
-    end,
-    mode = 'tabs',
-    truncate_names = false,
-    max_prefix_length = 100,
-    max_name_length = 100,
-    show_buffer_icons = false,
-    show_buffer_close_icons = false,
-    show_close_icon = false,
-    show_tab_indicators = false
-  },
+	options = {
+		show_duplicate_prefix = true,
+		close_command = function(n)
+			require("mini.bufremove").delete(n, false)
+		end,
+		right_mouse_command = function(n)
+			require("mini.bufremove").delete(n, false)
+		end,
+		diagnostics = "nvim_lsp",
+		always_show_bufferline = false,
+		offsets = {
+			{
+				filetype = "NvimTree",
+				-- filetype = "neo-tree",
+				text = "File-explorer",
+				highlight = "Directory",
+				text_align = "left",
+			},
+		},
+		hover = {
+			enabled = true,
+			delay = 100,
+			reveal = { "close" },
+		},
+		indicator = {
+			style = "underline",
+		},
+		numbers = function(opts)
+			return string.format("%s", opts.raise(opts.ordinal))
+		end,
+		mode = "tabs",
+		truncate_names = false,
+		max_prefix_length = 100,
+		max_name_length = 100,
+		show_buffer_icons = false,
+		show_buffer_close_icons = false,
+		show_close_icon = false,
+		show_tab_indicators = false,
+		name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
+			-- remove extension from markdown files for example
+			if buf.name:match("%.md") then
+				return vim.fn.fnamemodify(buf.name, ":t:r")
+			end
+			-- Use the file's full path and modify it as you like
+			local parent_folder = vim.fn.fnamemodify(buf.path, ":p:h:t")
+			local filename = vim.fn.fnamemodify(buf.path, ":t")
+			return parent_folder .. "/" .. filename -- includes only the direct parent and filename
+		end,
+	},
 })
