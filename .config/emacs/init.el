24ca2248b9fa5b4cdc652139a810f26dcac40fb2
carywill2 <carywill.k1@gmail.com>
2024-03-02 14:20:08 +0800

    emacs sync

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -22,7 +22,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil)))
+ '(package-selected-packages '(org-bullets evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -41,6 +41,8 @@
 (eval-after-load "org"
   '(require 'ox-md nil t))
 (setq org-agenda-files '("~/Library/Mobile Documents/com~apple~CloudDocs/Plain Org"))
+(setq org-clock-persist 'history)
+(org-clock-persistence-insinuate)
 
 ; auto refresh buffer
 (global-auto-revert-mode t)
@@ -49,3 +51,18 @@
 (menu-bar-mode -1)
 ; (scroll-bar-mode -1)
 (tool-bar-mode -1)
+
+; copy & paste
+(unless (package-installed-p 'pbcopy)
+  (package-install 'pbcopy))
+(require 'pbcopy)
+(turn-on-pbcopy)
+
+; org-bullets
+(unless (package-installed-p 'org-bullets)
+  (package-install 'org-bullets))
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+; backup dir
+(setq backup-directory-alist '(("." . "~/tmp")))
---
+++ .config/emacs/org-clock-save.el
@@ -0,0 +1,1 @@
+;; org-clock-save.el - Cary.local at <2024-03-02 Sat 14:20>
