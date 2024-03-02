e93fc47b24edee65eba56b589ac2fd91cbb8d308
carywill2 <carywill.k1@gmail.com>
2024-03-02 17:12:21 +0800

    emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -53,6 +53,8 @@
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
+; add done task time
+(setq org-log-done 'time)
 
 ; org roam
 (use-package org-roam
