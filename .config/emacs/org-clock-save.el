78ecc654292fc639add8e40558607336ba636885
carywill2 <carywill.k1@gmail.com>
2024-03-02 22:46:22 +0800

    fix: udpate

--- .config/emacs/org-clock-save.el
+++ .config/emacs/org-clock-save.el
@@ -1,1 +1,2 @@
-;; org-clock-save.el - Cary.local at <2024-03-02 Sat 22:37>
+;; org-clock-save.el - Cary.local at <2024-03-02 Sat 22:46>
+(setq org-clock-stored-history '(("/Users/cary/Library/Mobile Documents/com~apple~CloudDocs/Plain Org/literate.org" . 112)))
