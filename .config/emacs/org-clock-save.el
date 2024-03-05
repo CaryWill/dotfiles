83534340b223d9bbf1dcaf8ba0c327c9047b5c6e
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-03-05 19:34:38 +0800

    打分卡样式

--- .config/emacs/org-clock-save.el
+++ .config/emacs/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-clock-save.el - Cary.local at <2024-03-03 Sun 21:39>
+;; org-clock-save.el - MacBook-Pro.local at <2024-03-05 Tue 19:29>
 (setq org-clock-stored-history '(("/Users/cary/Library/Mobile Documents/com~apple~CloudDocs/Plain Org/literate.org" . 112)))
--- .config/nvim/after/plugin/image.lua
+++ .config/nvim/after/plugin/image.lua
@@ -1,44 +1,44 @@
 local status, plugin = pcall(require, "image")
 if not status then
-  return
+	return
 end
 
 -- default config
 require("image").setup({
-  backend = "kitty",
-  integrations = {
-    markdown = {
-      enabled = true,
-      clear_in_insert_mode = true,
-      download_remote_images = true,
-      only_render_image_at_cursor = false,
-      filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-    },
-    neorg = {
-      enabled = false,
-      clear_in_insert_mode = false,
-      download_remote_images = true,
-      only_render_image_at_cursor = false,
-      filetypes = { "norg" },
-    },
-    -- actually, i can just <leader>oo
-    -- to open the image in a new buffer
-    -- to review the image
-    org = {
-      enabled = true,
-      clear_in_insert_mode = false,
-      download_remote_images = true,
-      only_render_image_at_cursor = false,
-      filetypes = { "org" },
-    },
-  },
-  max_width = nil,
-  max_height = nil,
-  max_width_window_percentage = nil,
-  max_height_window_percentage = nil,
-  window_overlap_clear_enabled = true,                                     -- toggles images when windows are overlapped
-  window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "nvim-tree" },
-  editor_only_render_when_focused = true,                                  -- auto show/hide images when the editor gains/looses focus
-  tmux_show_only_in_active_window = true,                                  -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-  hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
+	backend = "kitty",
+	integrations = {
+		markdown = {
+			enabled = true,
+			clear_in_insert_mode = true,
+			download_remote_images = true,
+			only_render_image_at_cursor = false,
+			filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+		},
+		neorg = {
+			enabled = false,
+			clear_in_insert_mode = false,
+			download_remote_images = true,
+			only_render_image_at_cursor = false,
+			filetypes = { "norg" },
+		},
+		-- actually, i can just <leader>oo
+		-- to open the image in a new buffer
+		-- to review the image
+		org = {
+			enabled = true,
+			clear_in_insert_mode = false,
+			download_remote_images = true,
+			only_render_image_at_cursor = true,
+			filetypes = { "org" },
+		},
+	},
+	max_width = nil,
+	max_height = nil,
+	max_width_window_percentage = nil,
+	max_height_window_percentage = nil,
+	window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
+	window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "nvim-tree" },
+	editor_only_render_when_focused = true, -- auto show/hide images when the editor gains/looses focus
+	tmux_show_only_in_active_window = true, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+	hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
 })
