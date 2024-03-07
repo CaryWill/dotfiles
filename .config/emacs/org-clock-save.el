9eb4265355b0236f4531469b69ebb190d76c6b45
carywill2 <carywill.k1@gmail.com>
2024-03-07 22:57:20 +0800

    fix: update

--- .config/emacs/org-clock-save.el
+++ .config/emacs/org-clock-save.el
@@ -1,1 +1,2 @@
-;; org-clock-save.el - MacBook-Pro.local at <2024-03-07 Thu 13:39>
+;; org-clock-save.el - Cary.local at <2024-03-07 Thu 22:57>
+(setq org-clock-stored-history '(("/Users/cary/Library/Mobile Documents/com~apple~CloudDocs/Plain Org/literate.org" . 112)))
