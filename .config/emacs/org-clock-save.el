c8ceb1bbdc6773591c0379a127a69e7afc3540c8
carywill2 <carywill.k1@gmail.com>
2024-03-08 20:48:04 +0800

    emacs

--- .config/emacs/org-clock-save.el
+++ .config/emacs/org-clock-save.el
@@ -1,2 +1,7 @@
+<<<<<<< Updated upstream
 ;; org-clock-save.el - MacBook-Pro.local at <2024-03-08 Fri 11:12>
 (setq org-clock-stored-history '(("/Users/cary/Library/Mobile Documents/com~apple~CloudDocs/Plain Org/work.org" . 626) ("/Users/cary/Library/Mobile Documents/com~apple~CloudDocs/Plain Org/work.org" . 1504)))
+=======
+;; org-clock-save.el - Cary.local at <2024-03-08 Fri 20:47>
+(setq org-clock-stored-history '(("/Users/cary/Library/Mobile Documents/com~apple~CloudDocs/Plain Org/literate.org" . 112)))
+>>>>>>> Stashed changes
