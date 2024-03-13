214b410295a3af504a637a875a54e3541453b8b5
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-03-13 20:22:33 +0800

    refactor

--- .config/emacs/org-clock-save.el
+++ .config/emacs/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-clock-save.el - MacBook-Pro.local at <2024-03-13 Wed 14:36>
+;; org-clock-save.el - MacBook-Pro.local at <2024-03-13 Wed 14:38>
 (setq org-clock-stored-history '(("/Users/cary/Library/Mobile Documents/com~apple~CloudDocs/Plain Org/work.org" . 1)))
