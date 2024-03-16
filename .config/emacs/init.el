5e821fb3b505e97422b68adf62b385fee8dae3f6
carywill2 <carywill.k1@gmail.com>
2024-03-16 17:17:38 +0800

    add code css

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -134,7 +134,7 @@
 ; 并且将所有相对路径的图片放到当前目录下的 assets 目录
 ; 这样你可以直接将导出的 html 或者 md 文件放进去打包
 ; 然后就可以导入到yuque中了
-(defun add-lazy-loading-to-img-tags (html)
+(defun copy-files-from-img-tags (html)
   "Add loading=\"lazy\" attribute to img tags in the HTML content and copy local images to a new folder."
   (let* ((org-buffer (current-buffer))
          (default-directory (file-name-directory (buffer-file-name org-buffer)))
@@ -146,23 +146,35 @@
       (while (re-search-forward "<img +\\([^>]*?\\)src=\"\\([^\"]+\\)\"\\([^>]*\\)>" nil t)
         (let* ((match-string (match-string 0))
                (img-attrs (match-string 1))
-               (img-src (match-string 2))
+               (img-src (expand-file-name (match-string 2) default-directory)) ;; Get absolute path
                (img-rest (match-string 3)))
           (if (file-name-absolute-p img-src)
-              ;; If the image source is absolute, just add loading="lazy" attribute
-              (replace-match (concat "<img " img-attrs "src=\"" img-src "\" loading=\"lazy\"" img-rest))
-            ;; If the image source is relative, copy the image to the output folder and add loading="lazy" attribute
-            (let ((src-file (expand-file-name img-src default-directory)))
-              (when (file-exists-p src-file)
-                (let ((dest-file (expand-file-name (file-name-nondirectory img-src) output-folder)))
-                  (copy-file src-file dest-file t)
-                  (replace-match (concat "<img " img-attrs "src=\"" (file-relative-name dest-file default-directory) "\" loading=\"lazy\"" img-rest))))))))
+              ;; If the image source is relative, copy the image to the output folder and add loading="lazy" attribute
+              (progn
+                (let ((src-file img-src))
+                  (when (file-exists-p src-file)
+                    (let ((dest-file (expand-file-name (file-name-nondirectory img-src) output-folder)))
+                      (copy-file src-file dest-file t))
+                    ;; Replace the img tag with lazy loading attribute
+                    (replace-match (concat "<img " img-attrs "src=\"" (concat "assets/" (file-name-nondirectory img-src)) "\" loading=\"lazy\"" img-rest) t nil nil)))))
+          )
+        )
       (buffer-string))))
+
+(defun add-lazy-loading-to-img-tags (html)
+  "Add loading=\"lazy\" attribute to img tags in the HTML content."
+  (with-temp-buffer
+    (insert html)
+    (goto-char (point-min))
+    (while (re-search-forward "<img\\([^>]*\\)>" nil t)
+      (replace-match "<img\\1 loading=\"lazy\">"))
+    (buffer-string)))
 
 (defun return-modified-html-content (contents backend info)
   "Return HTML content with added lazy loading to img tags."
   (when (eq backend 'html)
-    (add-lazy-loading-to-img-tags contents)))
+    (add-lazy-loading-to-img-tags contents)
+    (copy-files-from-img-tags contents)))
 
 (add-to-list 'org-export-filter-final-output-functions 'return-modified-html-content)
 ; ---- end ----
