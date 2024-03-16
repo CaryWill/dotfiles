cbb8bf32389608c88a44ac6648e7c2799a342c78
carywill2 <carywill.k1@gmail.com>
2024-03-16 16:31:24 +0800

    add code css

--- .config/emacs/org-clock-save.el
+++ .config/emacs/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-clock-save.el - Cary.local at <2024-03-16 Sat 15:53>
+;; org-clock-save.el - Cary.local at <2024-03-16 Sat 16:31>
 (setq org-clock-stored-history '(("/Users/cary/Library/Mobile Documents/com~apple~CloudDocs/Plain Org/work.org" . 1)))
