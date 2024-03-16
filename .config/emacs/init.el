8c63123b618ed9892799954f37ff81a83520dec6
carywill2 <carywill.k1@gmail.com>
2024-03-16 19:34:26 +0800

    add code css

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -174,3 +174,47 @@
 
 (add-to-list 'org-export-filter-final-output-functions 'return-modified-html-content)
 ; ---- end ----
+
+; add video link - bilibili
+; http://endlessparentheses.com/embedding-youtube-videos-with-org-mode-links.html
+(defvar bilibili-iframe-format
+    ;; You may want to change your width and height.
+  (concat "<iframe src=\"%s\&autoplay=0&danmaku=1&high_quality=1\"
+          border=\"0\"
+          frameborder=\"no\"
+          framespacing=\"0\" 
+          sandbox=\"allow-top-navigation allow-same-origin allow-forms allow-scripts\"
+          scrolling=\"no\">
+          </iframe>"))
+
+(org-add-link-type
+ "bilibili"
+  (lambda (handle)
+   (browse-url handle))
+ (lambda (path desc backend)
+   (cl-case backend
+     (html (format bilibili-iframe-format
+                   path (or desc "")))
+     (latex (format "\href{%s}{%s}"
+                    path (or desc "video"))))))
+
+; add video link - youtube
+(defvar yt-iframe-format
+  ;; You may want to change your width and height.
+  (concat "<iframe"
+          " src=\"%s\""
+          " frameborder=\"0\""
+          " allowfullscreen>%s</iframe>"))
+
+(org-add-link-type
+ "yt"
+ (lambda (handle)
+   (browse-url
+    (concat ""
+            handle)))
+ (lambda (path desc backend)
+   (cl-case backend
+     (html (format yt-iframe-format
+                   path (or desc "")))
+     (latex (format "\href{%s}{%s}"
+                    path (or desc "video"))))))
--- .config/emacs/org-clock-save.el
+++ .config/emacs/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-clock-save.el - Cary.local at <2024-03-16 Sat 17:24>
+;; org-clock-save.el - Cary.local at <2024-03-16 Sat 19:33>
 (setq org-clock-stored-history '(("/Users/cary/Library/Mobile Documents/com~apple~CloudDocs/Plain Org/work.org" . 1)))
