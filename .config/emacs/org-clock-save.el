0148db3bb71ca3f4d12e27486b08499c77c0f024
carywill2 <carywill.k1@gmail.com>
2024-03-17 17:04:32 +0800

    fix: update

--- .config/emacs/org-clock-save.el
+++ .config/emacs/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-clock-save.el - Cary.local at <2024-03-17 Sun 17:02>
+;; org-clock-save.el - Cary.local at <2024-03-17 Sun 17:03>
 (setq org-clock-stored-history '(("/Users/cary/Library/Mobile Documents/com~apple~CloudDocs/Plain Org/work.org" . 1)))
