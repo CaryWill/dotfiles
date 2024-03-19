fee6ff8fae5f74e60e69a412f79b42166e6b3c45
carywill2 <carywill.k1@gmail.com>
2024-03-19 21:53:03 +0800

    fix: update

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -23,7 +23,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(spacemacs-theme solarized-theme org-bullets evil)))
+ '(package-selected-packages '(pbcopy spacemacs-theme solarized-theme org-bullets evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
