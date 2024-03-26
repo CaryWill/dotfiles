672462738c4817e9d718e88fa0dee1fc24a7c5b4
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-03-26 13:04:23 +0800

    fix: update

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -63,7 +63,8 @@
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
-(global-set-key (kbd "C-c n") 'tab-next)
+(global-set-key (kbd "C-c H") 'tab-previous)
+(global-set-key (kbd "C-c L") 'tab-next)
 ; add done task time
 (setq org-log-done 'time)
 (setq org-image-actual-width '600)
