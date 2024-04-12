3d09fdc65a692e7d05356dbcc9363e483d314b1d
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-04-12 14:53:58 +0800

    u

--- .config/emacs/orgmode-html-export.el
+++ .config/emacs/orgmode-html-export.el
@@ -50,7 +50,7 @@
 (defun return-modified-html-content (contents backend info)
   "Return HTML content with added lazy loading to img tags."
   (when (eq backend 'html)
-    ; (copy-files-from-img-tags contents)
+    (copy-files-from-img-tags contents)
     (add-lazy-loading-to-img-tags contents)))
 
 (add-to-list 'org-export-filter-final-output-functions 'return-modified-html-content)
