643ee85072f366dc2cf0c1256ffd3ce38b43b731
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-09-09 15:44:26 +0800

    fix:

--- .config/nvim/after/plugin/theme.rc.lua
+++ .config/nvim/after/plugin/theme.rc.lua
@@ -1,42 +1,47 @@
-local auto_dark_mode = require('auto-dark-mode')
+local status, plugin = pcall(require, "auto-dark-mode")
+if not status then
+	return
+end
+
+local auto_dark_mode = plugin
 
 function rosedark()
-  vim.cmd('colorscheme rose-pine-moon')
-  -- kitty
-  local job = require("plenary.job")
-  job:new({
-    command = "kitty",
-    args = { "+kitten", "themes", "--reload-in", "all", "Rosé Pine Moon" },
-  }):start()
+	vim.cmd("colorscheme rose-pine-moon")
+	-- kitty
+	local job = require("plenary.job")
+	job:new({
+		command = "kitty",
+		args = { "+kitten", "themes", "--reload-in", "all", "Rosé Pine Moon" },
+	}):start()
 end
 
 function jellybeansdark()
-  vim.cmd('colorscheme jellybeans-nvim')
-  -- kitty
-  local job = require("plenary.job")
-  job:new({
-    command = "kitty",
-    args = { "+kitten", "themes", "--reload-in", "all", "Rosé Pine Moon" },
-  }):start()
+	vim.cmd("colorscheme jellybeans-nvim")
+	-- kitty
+	local job = require("plenary.job")
+	job:new({
+		command = "kitty",
+		args = { "+kitten", "themes", "--reload-in", "all", "Rosé Pine Moon" },
+	}):start()
 end
 
 auto_dark_mode.setup({
-  -- update_interval = 1000 * 60,
-  update_interval = 1000,
-  set_dark_mode = function()
-    vim.api.nvim_set_option_value('background', 'dark', {})
-    -- rosedark()
-    jellybeansdark()
-  end,
-  set_light_mode = function()
-    vim.api.nvim_set_option_value('background', 'light', {})
-    vim.cmd('colorscheme rose-pine-dawn')
+	-- update_interval = 1000 * 60,
+	update_interval = 1000,
+	set_dark_mode = function()
+		vim.api.nvim_set_option_value("background", "dark", {})
+		-- rosedark()
+		jellybeansdark()
+	end,
+	set_light_mode = function()
+		vim.api.nvim_set_option_value("background", "light", {})
+		vim.cmd("colorscheme rose-pine-dawn")
 
-    -- kitty
-    local job = require("plenary.job")
-    job:new({
-      command = "kitty",
-      args = { "+kitten", "themes", "--reload-in", "all", "Rosé Pine Dawn" },
-    }):start()
-  end,
+		-- kitty
+		local job = require("plenary.job")
+		job:new({
+			command = "kitty",
+			args = { "+kitten", "themes", "--reload-in", "all", "Rosé Pine Dawn" },
+		}):start()
+	end,
 })
