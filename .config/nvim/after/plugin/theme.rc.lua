9c3985c10d617335e52600b107fbc7b90f03fbd4
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-09-12 10:31:49 +0800

    fix: update

--- .config/nvim/after/plugin/theme.rc.lua
+++ .config/nvim/after/plugin/theme.rc.lua
@@ -16,6 +16,7 @@ function rosedark()
 end
 
 function jellybeansdark()
+	vim.api.nvim_set_option_value("background", "dark", {})
 	vim.cmd("colorscheme jellybeans-nvim")
 	-- kitty
 	local job = require("plenary.job")
@@ -25,23 +26,26 @@ function jellybeansdark()
 	}):start()
 end
 
+function roseLight()
+	vim.api.nvim_set_option_value("background", "light", {})
+	vim.cmd("colorscheme rose-pine-dawn")
+
+	-- kitty
+	local job = require("plenary.job")
+	job:new({
+		command = "kitty",
+		args = { "+kitten", "themes", "--reload-in", "all", "Rosé Pine Dawn" },
+	}):start()
+end
+
 auto_dark_mode.setup({
 	-- update_interval = 1000 * 60,
 	update_interval = 1000,
 	set_dark_mode = function()
-		vim.api.nvim_set_option_value("background", "dark", {})
-		-- rosedark()
 		jellybeansdark()
 	end,
 	set_light_mode = function()
-		vim.api.nvim_set_option_value("background", "light", {})
-		vim.cmd("colorscheme rose-pine-dawn")
-
-		-- kitty
-		local job = require("plenary.job")
-		job:new({
-			command = "kitty",
-			args = { "+kitten", "themes", "--reload-in", "all", "Rosé Pine Dawn" },
-		}):start()
+		-- roseLight()
+		jellybeansdark()
 	end,
 })
