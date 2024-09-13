5d99085f4d44f7f7f402daaf49abbdbd43abd000
cary <cary@carys-MacBook-Pro.local>
2024-09-13 22:22:24 +0800

    fix: udapte

--- .config/nvim/after/plugin/lualine.rc.lua
+++ .config/nvim/after/plugin/lualine.rc.lua
@@ -1,53 +1,59 @@
 local status, plugin = pcall(require, "lualine")
 if not status then
-	return
+  return
 end
 
 local inited_db = false
 local function get_backlines()
-	if vim.bo.filetype == "org" then
-		if inited_db ~= true then
-			inited_db = true
-			require("org-roam").org_roam_buffer_toggle({ open_loc = false })
-		end
-		local file_path = vim.fn.expand("%:p")
-		local backlinks_count = vim.g.org_roam_backlinks[file_path]
-		if backlinks_count == 0 then
-			return backlinks_count or "no backlines"
-		else
-			return backlinks_count .. " backlines"
-		end
-	else
-		return ""
-	end
+  if vim.bo.filetype == "org" then
+    if inited_db ~= true then
+      inited_db = true
+      require("org-roam").org_roam_buffer_toggle({ open_loc = false })
+    end
+    local file_path = vim.fn.expand("%:p")
+    local backlinks_count = vim.g.org_roam_backlinks[file_path]
+    if backlinks_count == 0 then
+      return backlinks_count or "no backlines"
+    else
+      return backlinks_count .. " backlines"
+    end
+  else
+    return ""
+  end
 end
 
 plugin.setup({
-	options = {
-		-- disabled_filetypes = { 'packer', 'NVimTree', "sagaoutline", "neo-tree" },
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-	},
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch", { "filename", path = 1 } },
-		lualine_c = {
-			"diagnostics",
-		},
-		lualine_x = {},
-		lualine_y = {},
-		lualine_z = {
-			-- get_backlines,
-			"filesize",
-			"filetype",
-		},
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { { "filename", path = 1 } },
-		lualine_x = { "filesize", "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
+  options = {
+    -- disabled_filetypes = { 'packer', 'NVimTree', "sagaoutline", "neo-tree" },
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    -- echo &ft 可以看当前文件类型
+    disabled_filetypes = {
+      "dapui_watches", "dapui_breakpoints",
+      "dapui_scopes", "dapui_console",
+      "dapui_stacks", "dap-repl"
+    }
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch", { "filename", path = 1 } },
+    lualine_c = {
+      "diagnostics",
+    },
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {
+      -- get_backlines,
+      "filesize",
+      "filetype",
+    },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { { "filename", path = 1 } },
+    lualine_x = { "filesize", "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
 })
