c91d92b29868098cea3923032f9860d5454d4b86
cary <cary@carys-MacBook-Pro.local>
2024-09-16 22:58:02 +0800

    ifx: upd

--- .config/nvim/after/plugin/lsp-saga.rc.lua
+++ .config/nvim/after/plugin/lsp-saga.rc.lua
@@ -1,37 +1,37 @@
 local status, plugin = pcall(require, "lspsaga")
 if not status then
-	return
+  return
 end
 
 plugin.setup({
-	ui = {
-		winblend = 10,
-		border = "rounded",
-		code_action = "",
-		actionfix = "💡  ",
-		devicon = false,
-	},
-	symbol_in_winbar = {
-		enable = false,
-	},
-	diagnostic = {
-		show_code_action = false,
-		extend_relatedInformation = true,
-	},
-	outline = {
-		auto_preview = false,
-		detail = false,
-	},
+  ui = {
+    winblend = 10,
+    border = "rounded",
+    code_action = "",
+    actionfix = "💡  ",
+    devicon = false,
+  },
+  symbol_in_winbar = {
+    enable = false,
+  },
+  diagnostic = {
+    show_code_action = false,
+    extend_relatedInformation = true,
+  },
+  outline = {
+    auto_preview = false,
+    detail = false,
+  },
 })
 
 -- diagnostics navigation
 vim.keymap.set("n", "[e", function()
-	-- require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
-	require("lspsaga.diagnostic"):goto_prev()
+  -- require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+  require("lspsaga.diagnostic"):goto_prev()
 end)
 vim.keymap.set("n", "]e", function()
-	-- require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
-	require("lspsaga.diagnostic"):goto_next()
+  -- require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
+  require("lspsaga.diagnostic"):goto_next()
 end)
 
 -- code action
