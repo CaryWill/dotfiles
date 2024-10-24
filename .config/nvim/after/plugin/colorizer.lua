e7d5f72a3f7570ad2fbf5517d9c1f1a12a79bb8f
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-10-24 11:22:55 +0800

    fix: freeze when telescoping

--- .config/nvim/after/plugin/colorizer.lua
+++ .config/nvim/after/plugin/colorizer.lua
@@ -1,4 +1,20 @@
-local status, plugin = pcall(require, "colorizer")
-if (not status) then return end
+-- perf problem solved
+-- https://github.com/nvim-telescope/telescope.nvim/issues/1379#issuecomment-1208003388
+local ok, colorizer = pcall(require, "colorizer")
+if not ok then
+	return
+end
 
-plugin.setup()
+-- https://github.com/lilydjwg/colorizer
+colorizer.setup({ "css", "javascript", "lua", "vim", "toml", "svelte", "typescript" }, {
+	RGB = true, -- #RGB hex codes
+	RRGGBB = true, -- #RRGGBB hex codes
+	names = false, -- "Name" codes like Blue oe blue
+	RRGGBBAA = true, -- #RRGGBBAA hex codes
+	rgb_fn = true, -- CSS rgb() and rgba() functions
+	hsl_fn = true, -- CSS hsl() and hsla() functions
+	css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+	css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+	-- Available modes: foreground, background, virtualtext
+	mode = "background", -- Set the display mode.)
+})
