8f279b085b2625eb0268f618fc18921263ef5350
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-10-25 15:19:17 +0800

    hardtiime

--- .config/nvim/after/plugin/hardtime.lua
+++ .config/nvim/after/plugin/hardtime.lua
@@ -2,4 +2,14 @@ local status, plugin = pcall(require, "hardtime")
 if not status then
 	return
 end
-require("hardtime").setup()
+
+require("hardtime").setup({
+	disable_mouse = false,
+	disabled_keys = {
+		["<Up>"] = { "", "i" },
+		["<Down>"] = { "", "i" },
+		["<Left>"] = { "", "i" },
+		["<Right>"] = { "", "i" },
+	},
+	-- restriction_mode = "hint",
+})
